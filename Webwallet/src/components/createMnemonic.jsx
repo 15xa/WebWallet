5822eecd9f0a94cd589e0ba1e15e0906d92ef569
Aditya <aditya.ps1515@gmail.com>
2025-01-21 16:34:33 +0530

    fetches mnemonic now

--- Webwallet/src/components/createMnemonic.jsx
+++ Webwallet/src/components/createMnemonic.jsx
@@ -2,28 +2,37 @@ import { generateMnemonic } from "bip39";
 import { useState, useEffect } from "react";
 import { Buffer } from 'buffer';
 
-if (typeof window !== "undefined") {
-    window.Buffer = Buffer;
-}
-const [error, setError] = useState(null); // Add this to your component
+
 
 export const CreateMnemonic = () => {
+
+
+    const[loading, setLoading] = useState("false") 
+
+    if (typeof window !== "undefined") {
+        window.Buffer = Buffer;
+    }
+    const [error, setError] = useState(null);
 
     const [mnemonic, setMnemonic] = useState("");
 
     useEffect(() => {
+        setLoading("true");
         const fetchMnemonic = async () => {
-          try {
-            console.log("Fetching mnemonic...");
-            const generatedMnemonic = await generateMnemonic();
-            console.log("Mnemonic generated:", generatedMnemonic);
-            setMnemonic(generatedMnemonic);
-            setLoading(false);
-          } catch (err) {
-            console.error("Error generating mnemonic:", err);
-            setError("Failed to generate mnemonic.");
-            setLoading(false);
-          }
+            await setTimeout(async () => {
+                try {
+                    console.log("Fetching mnemonic...");
+                    const generatedMnemonic = await generateMnemonic();
+                    console.log("Mnemonic generated:", generatedMnemonic);
+                    setMnemonic(generatedMnemonic);
+                    setLoading(false);
+                  } catch (err) {
+                    console.error("Error generating mnemonic:", err);
+                    setError("Failed to generate mnemonic.");
+                    setLoading(false);
+                  }
+            }, 1500);
+          
         };
         fetchMnemonic();
         
