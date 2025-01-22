035055220880882f9cfc37bd62c504f27c915440
Aditya <aditya.ps1515@gmail.com>
2025-01-22 21:36:55 +0530

    creation route working, [links soon]

--- Webwallet/src/App.jsx
+++ Webwallet/src/App.jsx
@@ -3,11 +3,50 @@
   import { CreateMnemonic } from './components/createMnemonic';
 
   function App() {
+<<<<<<< HEAD
     const [count, setCount] = useState(0)
+=======
+
+
+    const [mnemonic, setMnemonic] = useState("");
+    const [givenMnemonic, setGivenMnemonic] = useState("");
+    const [createdMnemonic, setCreatedmnemonic] = useState("");
+>>>>>>> 9c9747c (creation route working, [links soon])
+
+    const Home = () => {
+
+
+      return(
+      <div className='flex flex-col justify-center items-center h-screen w-screen'>
+        <div className="text-2xl text-blue-800">Do you have your phrase?</div>
+        <input
+          type="text"
+          id="mnemonic"
+          placeholder="Enter the Mnemonic phrase"
+          value={mnemonic}
+          onChange={(e) => setGivenMnemonic(e.target.value)}
+          className="input-class border border-black border-1"
+        />
+        
+        {givenMnemonic && <Fetchwallet mnemonic={givenMnemonic} /> }
+         
+        <button className='' >Create new wallet</button>
+    
+      </div>
+        )
+  }
 
     return (
       <>
+<<<<<<< HEAD
       <CreateMnemonic></CreateMnemonic>
+=======
+      <BrowserRouter>
+        <Routes>
+          <Route path='/createmnemonic' element={<CreateMnemonic/>}></Route>
+          <Route path='/' element={<Home/>}></Route>
+          </Routes>      </BrowserRouter>
+>>>>>>> 9c9747c (creation route working, [links soon])
       
       </>
     )
--- Webwallet/src/components/createMnemonic.jsx
+++ Webwallet/src/components/createMnemonic.jsx
@@ -7,40 +7,52 @@ import { Buffer } from 'buffer';
 export const CreateMnemonic = () => {
 
 
-    const[loading, setLoading] = useState("false") 
+    const[loading, setLoading] = useState(false) ;
+    const[created, setCreated] = useState(false);
 
     if (typeof window !== "undefined") {
         window.Buffer = Buffer;
     }
-    const [error, setError] = useState(null);
 
-    const [mnemonic, setMnemonic] = useState("");
+    const [cmnemonic, setCmnemonic] = useState("");
+    
+    const updateCreation = () => {
+        if(cmnemonic){
+            setCreated(true);
+        }
+        else{
+            setCreated(false);
+        }
+    }
 
-    useEffect(() => {
-        setLoading("true");
-        const fetchMnemonic = async () => {
-            await setTimeout(async () => {
+        
+       const fetchMnemonic = async () => {
+            setLoading(true);
+            const timer = await setTimeout(async () => {
                 try {
                     console.log("Fetching mnemonic...");
                     const generatedMnemonic = await generateMnemonic();
                     console.log("Mnemonic generated:", generatedMnemonic);
-                    setMnemonic(generatedMnemonic);
+                    setCmnemonic(generatedMnemonic);
                     setLoading(false);
                   } catch (err) {
                     console.error("Error generating mnemonic:", err);
                     setError("Failed to generate mnemonic.");
                     setLoading(false);
                   }
+                  clearTimeout(timer);
+
             }, 1500);
           
         };
-        fetchMnemonic();
         
-    }, [])
+    
+
     
    return(
    <div>
-   {mnemonic ? `Mnemonic phase is: ${mnemonic}`: "generating....."}
+    <button onClick={fetchMnemonic} disabled={loading}>Create</button>
+   {cmnemonic ? `Mnemonic phase is: ${cmnemonic}`: "generating....."}
    </div> 
    )
 }
